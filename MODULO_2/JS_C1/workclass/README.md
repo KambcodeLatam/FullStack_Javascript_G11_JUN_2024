94ac3c0184d846de29218446755d4ddebc1fbcf7
Daniel Canon <danielycm02@gmail.com>
2024-07-22 21:22:46 -0500

    clase_1

---
+++ MODULO_2/JS_C1/workclass/README.md
@@ -0,0 +1,18 @@
+### Dias de clases
+Lunes, Miercoles y Viernes (7p.m - 9p.m)
+
+## Dias de Tutorias
+Tutorias son Opcionales (8p.m - 9p.m)
+Martes, Jueves
+
+### Entrega de Tareas
+Las entregas se van a realizar por el chat INTERNO con el profesor
+Siempre por las entregas de las tareas, voy a hacer retroalimentacion
+- JPG las tareas, entrega de la tarea en pantallazos
+- Tarea #1_Variables. Daniel Cañon (Ejemplo)
+
+### Metodologia de la clase
+- Coloco Retos en clases: Este reto si lo envian al grupo whatsapp (10min - 15min)
+
+### Clases
+- Al final de la clase, siempre hago el push, despues de 15 minutos de terminada la clase, les envio al grupo el link de la tarea y el link de la grabacion de la clase.
---
+++ MODULO_2/JS_C1/workclass/main.js
@@ -0,0 +1,59 @@
+// Tipos de Datos en JS
+
+// 1. String o Cadenas de Texto -> Con comillas
+// "CSS -> Es el estilo que le damos a nuestra pagina"
+// 'Kambcode'
+// "Kambcode"
+// "Yo tengo 45 años"
+
+// 2. Numericos -> Sin comillas
+// 45
+// 2024
+// 1000000
+// 3.1416
+
+// 3. Boleanos
+// True -> verdadero
+// False -> falso
+
+
+// Variables (var)
+// Es la forma de almacenar un dato para modificarlo, presentarlo o utilizarlo
+
+var precio = 49.08;
+document.write('El precio de los audifonos es: $', precio);
+
+// camelCase -> costoDeEnvio
+var costoDeEnvio = 10;
+var iva = 4;
+
+// Ejemplos String
+var nombreProducto = "Audifonos Negros";
+document.write("<br>", nombreProducto); // "Aufinos negros"
+
+// Redeclarar Variables
+nombreProducto = "Razer BlackShark V2 X Auriculares para gamer:";
+document.write("<br>", nombreProducto); // "Razer BlackShark V2 X Auriculares para gamer:"
+
+
+// Operar datos y operar variables
+// suma +
+// resta -
+// multiplicar *
+// dividir /
+
+var precioTotal = precio + costoDeEnvio + iva; // 49.08 + 10 + 4 = 63.08
+document.write("<br>", precioTotal);
+
+var precioTotalX2 = precioTotal * 2; // 126.16
+document.write("<br>",precioTotalX2)
+
+// alert("Esto es una alerta, porfavor dele aceptar para continuar");
+
+
+// prompt -> El usuario puede colocar cualquier cosa
+var nombreUsuario = prompt("Esta es una alerta. Porfavor escriba su nombre") // Daniel
+document.write("<br> Hola, bienvenido a la pagina, su nombre es: ", nombreUsuario);
+
+
+var altura = prompt("Porfavor digita tu altura en Cm")
