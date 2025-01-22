27ab453b2bb0a090a17f376614d1a831d774c854
usernow <usernow.82@gmail.com>
2025-01-22 08:57:37 -0500

    C3 SQL

---
+++ MODULO_6/BaseDeDatos-C3/workclass/DDL.sql
@@ -0,0 +1,19 @@
+-- DDL --
+
+-- CREATE 
+
+CREATE TABLE students (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR NOT NULL,
+	phone VARCHAR(10),
+	email VARCHAR UNIQUE
+);
+
+CREATE TABLE employees (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR NOT NULL,
+	position VARCHAR(50) NOT NULL,
+	salary DECIMAL(10, 2),
+	isActive BOOLEAN DEFAULT true,
+	hire_date DATE DEFAULT CURRENT_DATE
+);
